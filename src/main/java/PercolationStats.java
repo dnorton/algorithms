338ee233d03d385a54a96d4cd82c7e20bb3eb714
dnorton <daniel@dnorton.org>
2013-09-22 23:32:00 -0400

    fixed problem with percolates and implemented PercolationStats.runSimulation()

--- src/main/java/PercolationStats.java
+++ src/main/java/PercolationStats.java
@@ -3,9 +3,18 @@
  */
 public class PercolationStats {
 
+    private double[] results;
+    private int dimensionSize;
+    private int times; //number of times, T, to repeat the percolation steps
+
     // perform T independent computational experiments on an N-by-N grid
     public PercolationStats(int N, int T) {
 
+        if (N <= 0 || T <= 0) throw new IllegalArgumentException("N and T must be greater than 0");
+
+        dimensionSize = N;
+        times = T;
+        results = new double[T];
     }
 
     // sample mean of percolation threshold
@@ -29,6 +38,33 @@ public class PercolationStats {
     }
 
     public static void main(String[] args) {
+
+        int N = Integer.parseInt(args[0]);
+        int T = Integer.parseInt(args[1]);
+
+        PercolationStats stats = new PercolationStats(N, T);
+        stats.runSimulation();
+    }
+
+    private void runSimulation() {
+        for (int t = 0; t < results.length; t++) {
+            int count = 0;
+
+            Percolation percolation = new Percolation(dimensionSize);
+            while(!percolation.percolates()) {
+                int i = StdRandom.uniform(1, dimensionSize + 1);
+                int j = StdRandom.uniform(1, dimensionSize + 1);
+
+                while(percolation.isOpen(i, j)) {
+                    i = StdRandom.uniform(1, dimensionSize + 1);
+                    j = StdRandom.uniform(1, dimensionSize + 1);
+                }
+                percolation.open(i,j);
+                count++;
+
+            }
+            results[t] = ((double)count) / (dimensionSize * dimensionSize);
+        }
 
     }
 }
